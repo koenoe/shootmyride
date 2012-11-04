22305c7fbbaa1aeede534eae2d99f476a661502b
Koen Romers <info@koenromers.com>
2012-11-04 23:30:20 +0100

    Added lots of stuff

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,3 +1,5 @@
+var apiToken = 'sLv44JmNroYjmZvpzMTj';
+
 $(function(){
 	$('.cycle').cycle({
 		slides: '> figure',
@@ -16,4 +18,22 @@ $(function(){
 		$('html,body').animate({ scrollTop: 0 }, 300);
 		return false;
 	});
+
+	// TEST AJAX
+	// $.ajaxSetup({
+	// 	beforeSend: function(xhr) {
+	// 		xhr.setRequestHeader('X-CSRF-Token',$('meta[name="csrf-token"]').attr('content'));
+	// 	}
+	// });
+
+	// $.ajax({
+	// 	url: '/locomotive/api/content_types/slider/entries.json?auth_token=' + apiToken,
+	// 	type: 'get',
+	// 	dataType: 'json',
+	// 	success: function(response){
+	// 		console.log(response);
+	// 	}
+	// });
+	// END TEST AJAX
+
 });
