eccb9a4d60109f844cf77c0e1277a1f4abb70347
Koen Romers <info@koenromers.com>
2012-11-22 21:38:47 +0100

    Updated to new design

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,39 +1,28 @@
-var apiToken = 'sLv44JmNroYjmZvpzMTj';
-
 $(function(){
-	$('.cycle').cycle({
-		slides: '> figure',
-		fx: 'scrollHorz',
-		timeout: 5000,
-		speed: 300,
-		prev: '.slider-prev',
-		next: '.slider-next',
-		pager: '.slider-pager',
-		pagerTemplate: '<span></span>',
-		pagerActiveClass: 'active',
-		swipe: true
-	});
 
-	$('.back-to-top').click(function(){
-		$('html,body').animate({ scrollTop: 0 }, 300);
+	$('.royalslider').royalSlider({
+		arrowsNav: true,
+		controlNavigation: 'bullets',
+		keyboardNavEnabled: true,
+		controlsInside: false,
+		navigateByClick: false,
+		imageScaleMode: 'fill',
+		autoHeight: false,
+		autoScaleSlider: false, 
+		imageAlignCenter: true,
+		transitionType:'move',
+		slidesSpacing: 0,
+		loopRewind: true,
+		randomizeSlides: true,
+		autoPlay: {
+			enabled: true,
+			pauseOnHover: true
+		}
+	}); 
+
+	$('.backtotop').click(function(){
+		$('html,body').animate({ scrollTop: 0 }, 500);
 		return false;
 	});
-
-	// TEST AJAX
-	// $.ajaxSetup({
-	// 	beforeSend: function(xhr) {
-	// 		xhr.setRequestHeader('X-CSRF-Token',$('meta[name="csrf-token"]').attr('content'));
-	// 	}
-	// });
-
-	// $.ajax({
-	// 	url: '/locomotive/api/content_types/slider/entries.json?auth_token=' + apiToken,
-	// 	type: 'get',
-	// 	dataType: 'json',
-	// 	success: function(response){
-	// 		console.log(response);
-	// 	}
-	// });
-	// END TEST AJAX
 
 });
