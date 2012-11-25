efccf960428e92c04559b9ea08104c13c3b81aeb
Koen Romers <info@koenromers.com>
2012-11-25 21:08:45 +0100

    Added lots of stuff

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -20,8 +20,21 @@ $(function(){
 		}
 	}); 
 
+	$('figure.header').each(function(){
+		var src = $(this).data('src');
+		$(this).css({
+			backgroundImage: 'url('+src+')'
+		});
+	});
+
 	$('.backtotop').click(function(){
 		$('html,body').animate({ scrollTop: 0 }, 500);
+		return false;
+	});
+
+	var offset = $('.content-wrapper').offset();
+	$('.scrolldown').click(function(){
+		$('html,body').animate({ scrollTop: offset.top }, 500);
 		return false;
 	});
 
